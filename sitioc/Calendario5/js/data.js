62a1bb0d4af21b00299a8d947c3057926ab6beab
Jaime <loama18@gmail.com>
2015-11-24 20:09:47 -0600

    sitioc

---
+++ sitioc/Calendario5/js/data.js
@@ -0,0 +1,13 @@
+var codropsEvents = {
+	'11-23-2015' : '<a href="http://tympanus.net/codrops/2012/11/23/three-script-updates/">Three Script Updates</a>',
+	'11-21-2015' : '<a href="http://tympanus.net/codrops/2012/11/21/adaptive-thumbnail-pile-effect-with-automatic-grouping/">Adaptive Thumbnail Pile Effect with Automatic Grouping</a>',
+	'11-20-2015' : '<a href="http://tympanus.net/codrops/2012/11/20/learning-principles-for-improving-your-css/">Learning Principles for Improving Your CSS</a>',
+	'11-19-2015' : '<a href="http://tympanus.net/codrops/2012/11/19/responsive-css-timeline-with-3d-effect/">Responsive CSS Timeline with 3D Effect</a>',
+	'11-14-2015' : '<a id="django" href="http://tympanus.net/codrops/2012/11/14/creative-css-loading-animations/" style="background:#8e24aa"">Creative CSS Loading Animations</a>',
+	'12-31-2015' : '<span>New Year\'s Eve</span>'
+};
+
+
+//Material colors
+//Programming blue #039be5 hover #0288d1
+//Design purple #8e24aa hover #7b1fa2  
