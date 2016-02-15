fd41c31dae990850b5ab6432e83c217a74895481
Jaime <loama18@gmail.com>
2016-02-15 17:29:36 -0600

    404

--- sitioc/js/init.js
+++ sitioc/js/init.js
@@ -6,3 +6,19 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function SubmitForm()
+{
+     document.forms['contactus'].action='action1.php';
+     document.forms['contactus'].target='frame_result1';
+     document.forms['contactus'].submit();
+ 
+     document.forms['contactus'].action='action2.php';
+     document.forms['contactus'].target='frame_result2';
+     document.forms['contactus'].submit();
+
+     document.forms['contactus'].action='action2.php';
+     document.forms['contactus'].target='frame_result2';
+     document.forms['contactus'].submit();
+     return true;
+}
