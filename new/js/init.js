42d9b952d1ff1a9b02168ee2b7eb2927d1c04b46
Jaime <loama18@gmail.com>
2016-03-29 16:47:01 -0600

    added animation to header with 1.3s
    Added animation and background is now able to go all the body without
    interruption.
    Bug with chrome inspector.

---
+++ new/js/init.js
@@ -0,0 +1,23 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
