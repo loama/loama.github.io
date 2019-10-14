b918d90cba978916b4d5520e95a901f744362329
Jaime Lopez <loama18@gmail.com>
2019-10-14 17:53:44 -0500

    finish adding logos

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -6,7 +6,7 @@
       <ul class="experience-list">
 
         <li>
-          <div class="logo"></div>
+          <div class="logo zeel"></div>
 
           <div class="info">
             <div class="head">
@@ -23,7 +23,7 @@
         </li>
 
         <li>
-          <div class="logo"></div>
+          <div class="logo grupomodelo"></div>
 
           <div class="info">
             <div class="head">
@@ -34,6 +34,53 @@
               <span class="dates">Jan 18 - Apr 18</span>
             </div>
             <div class="details">Built -in 2 months- the first version of a beer subscription service for the biggest beer company in the world.</div>
+          </div>
+        </li>
+
+        <li>
+          <div class="logo eiya"></div>
+
+          <div class="info">
+            <div class="head">
+              <span class="title">
+                <a href="https://eiya.mx" target="_blank">Eiya.mx</a> - Lead Developer (Vue/Node/Python)
+              </span>
+
+              <span class="dates">Aug 16 - Oct 17</span>
+            </div>
+
+            <div class="details">Coded the messenger app, internal control panel, restaurant panel and customer facing website for tracking.</div>
+          </div>
+
+        </li>
+
+        <li>
+          <div class="logo rappi"></div>
+
+          <div class="info">
+            <div class="head">
+              <span class="title">
+                <a href="https://rappi.com" target="_blank">Rappi.com</a> - Full Stack Developer (jQuery/Python)
+              </span>
+
+              <span class="dates">Feb 16 - Aug 16</span>
+            </div>
+            <div class="details">Started the engineering team in Mexico and built the first version of the internal operations console.</div>
+          </div>
+        </li>
+
+        <li>
+          <div class="logo centraal"></div>
+
+          <div class="info">
+            <div class="head">
+              <span class="title">
+                <a href="https://centraal.com" target="_blank">Centraal.com</a> - Junior Full Stack Developer (jQuery/Python)
+              </span>
+
+              <span class="dates">Jun 15 - Feb 16</span>
+            </div>
+            <div class="details">Built an internal system to manage the coworking space and gave support to all the technology in the space.</div>
           </div>
         </li>
 
@@ -59,6 +106,10 @@ export default {
   @import "@/variables.sass"
 
   #experience
+
+    .section
+      padding: 0 8px
+
     ul.experience-list
       margin: 0
       padding: 0
@@ -70,10 +121,30 @@ export default {
           border-bottom: none
 
         .logo
+          background-position: center
+          background-repeat: no-repeat
+          background-size: contain
           display: inline-block
           height: 56px
-          vertical-align: top
-          width: 88px
+          margin: 0 8px
+          vertical-align: center
+          width: 72px
+
+          &.zeel
+            background-image: url('../assets/img/companies/zeel.svg')
+
+          &.grupomodelo
+            background-image: url('../assets/img/companies/grupomodelo.png')
+
+          &.eiya
+            background-image: url('../assets/img/companies/eiya.png')
+
+          &.rappi
+            background-image: url('../assets/img/companies/rappi.png')
+
+          &.centraal
+            background-size: 48px
+            background-image: url('../assets/img/companies/centraal.png')
 
         .info
           display: inline-block
