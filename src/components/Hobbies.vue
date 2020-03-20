00216b74731cc800d1c0cce16aecc88f93cc6580
Jaime López <loama18@gmail.com>
2020-03-19 18:47:52 -0600

    improve countries img

--- src/components/Hobbies.vue
+++ src/components/Hobbies.vue
@@ -3,7 +3,7 @@
       <div class="section-name"> Hobbies </div>
 
       <div class="section">
-        <img class="visited_countries" src="../assets/img/visited_countries.png">
+        <img class="visited_countries" src="../assets/img/visited_countries.jpg">
 
         <span> I love traveling, have been to 42 countries so far. I also enjoy bicycling, riding my monocycle, going to hackathons and petting doggos. </span>
         <img src="../assets/img/doggo.png" class="doggo">
